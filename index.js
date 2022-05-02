183e38a307e14091c18fac72121552a2b2ee16d1
Wonbin Lee <lob3767@gmail.com>
2022-05-02 21:22:10 +0900

    [Create] User SignUp control

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
+const config = require('./config/key');
+const { User } = require('./models/User');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
 mongoose
-    .connect(
-        'mongodb+srv://onbin:lh864235@boilerplate.ltrzt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-    )
+    .connect(config.mongoURI)
     .then(() => console.log('MongoDB Connected..'))
     .catch((err) => console.log(err));
 
 app.get('/', (req, res) => res.send('Hello World'));
+
+app.post('/register', (req, res) => {
+    const user = new User(req.body);
+    user.save((err, doc) => {
+        if (err) return res.json({ success: false, err });
+        return res.status(200).json({
+            success: true,
+        });
+    });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
